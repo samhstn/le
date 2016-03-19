5e8c2f83b29554f58d5f2e98a5620aa6d37837e8
shouston3 <shouston3@me.com>
2016-03-19 15:41:40 +0000

    server serves the correct file when built, react is up and running in dev mode, first Hello World with React from the Server

--- front/app.js
+++ front/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World!')
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Components from './components.js'
+
+const rootElement = document.getElementById('container')
+
+ReactDOM.render(
+  <Components />,
+  rootElement
+)
