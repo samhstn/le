494f961ada01a195425c558827b53d5bf8131e66
shouston3 <shouston3@me.com>
2016-03-19 16:40:05 +0000

    made initial lang button

---
+++ front/langbutton.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+class LangButton extends React.Component {
+  render () {
+    styles.backgroundColor = this.props.backgroundColor
+    return <div style={styles}>{this.props.text}</div>
+  }
+}
+
+LangButton.propTypes = {
+  backgroundColor: React.PropTypes.string,
+  text: React.PropTypes.string
+}
+
+LangButton.defaultProps = {
+  backgroundColor: 'red',
+  text: 'hello'
+}
+
+var styles = {
+  fontSize: '5vmin',
+  border: '1px solid black',
+  textAlign: 'center',
+  width: '48%',
+  height: '12%',
+  borderRadius: '5%',
+  paddingTop: '2%'
+}
+
+export default LangButton
