c1b3bd3c96bee00cb5b6fc9e1dbf9a42c537d8ad
shouston3 <shouston3@me.com>
2016-03-19 16:39:20 +0000

    made initial nav button

---
+++ front/navbutton.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+class NavButton extends React.Component {
+  render () {
+    return <div style={styles}>{this.props.text}</div>
+  }
+}
+
+NavButton.propTypes = {
+  text: React.PropTypes.string
+}
+
+NavButton.defaultProps = {
+  text: 'Home'
+}
+
+const styles = {
+  fontSize: '36px',
+  border: '1px solid black',
+  backgroundColor: 'brown',
+  width: '12%',
+  height: '5%',
+  padding: '4%',
+  textAlign: 'center',
+  position: 'absolute',
+  borderRadius: '50%'
+}
+
+export default NavButton
