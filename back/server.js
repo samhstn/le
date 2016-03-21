9f6d8bce601997784f77650253defb83b98d74ca
shouston3 <shouston3@me.com>
2016-03-21 22:00:58 +0000

    reconfigure server

--- back/server.js
+++ back/server.js
@@ -1,12 +1,10 @@
 'use strict'
-
 // node modules
 const Hapi = require('hapi')
 const Inert = require('inert')
-
 // server config
 const server = new Hapi.Server()
-const port = 8080
+const port = 4000
 
 server.connection({
   port: port
@@ -19,18 +17,34 @@ const plugins = [
 
 server.register(plugins, (err) => {
   if (err) {
-    console.log(err)
     throw err
   }
-  server.route([{
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-      directory: {
-        path: 'front/production'
+  server.route([
+    {
+      method: 'GET',
+      path: '/',
+      handler: (request, reply) => {
+        const path = __dirname.join('../../frontend/production/index.html')
+        console.log(path)
+        reply.file(path)
+      }
+    }, {
+      method: 'GET',
+      path: '/',
+      handler: (request, reply) => {
+        const client = require('./redis.js')
+        require('./populateDB/populateDB.js')(client) // link these up to the correct urls, plan, then make work!
+        reply.redirect('/')
+      }
+    }, {
+      method: 'GET',
+      path: '/',
+      handler: (request, reply) => {
+        const path = __dirname.join('./../../frontend/production/amazon.js')
+        reply.file(path)
       }
     }
-  }])
+  ])
 })
 
 module.exports = server
