b6abaa1115d371fa57c86e7a364b68679ad3af31
shouston3 <shouston3@me.com>
2016-03-22 09:35:31 +0000

    modifying scripts for ease of testing (travis or not) and modifying the test file to run particular tests

--- backTest/test.js
+++ backTest/test.js
@@ -1,12 +1,1 @@
-var tape = require('tape')
-var server = require('../back/server.js')
-
-tape('Does server respond successfully with the index?', function (t) {
-  server.inject({method: 'GET', url: '/'}, function (res) {
-    t.equal(res.statusCode, 200, 'Server responds with statusCode: 200')
-    t.ok(res.payload.match('<!DOCTYPE html>'), 'file starts with <!DOCTYPE html>')
-    t.ok(res.payload.match('id="container"'), 'index contains the container id')
-    t.ok(res.payload.match('<script src="app.js">'), 'index is linking to app.js')
-    t.end()
-  })
-})
+require('./server.test.js')
