b969262f2d88a9c2f6b7442c880a026128adf65e
shouston3 <shouston3@me.com>
2016-03-26 16:07:14 +0000

    changing new layout to not use react router

--- front/app.js
+++ front/app.js
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Routes from './routes.js'
-// import Components from './components.js'
+import Components from './components.js'
 
 const rootElement = document.getElementById('container')
 
 ReactDOM.render(
-  <Routes />,
+  <Components />,
   rootElement
 )
