6307629c0bbbf65d4f77d5be8bb95223373b8edd
shouston3 <shouston3@me.com>
2016-03-28 21:35:54 +0100

    further changes to router, may need to implement redux next

--- front/index.js
+++ front/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 
 const rootElement = document.getElementById('container')
 
-render((
-  <Router history={browserHistory} route={routes} />
+ReactDOM.render((
+  <Router history={browserHistory} routes={routes} />
 ), rootElement);
