065db1f0d705723df2caaee46badecfb7ebe8b10
shouston3 <shouston3@me.com>
2016-05-26 22:06:17 +0200

    refactored server test to not include quite pointless tests, now do a foreach on an array of endpoints

--- back/backTest/server.test.js
+++ back/backTest/server.test.js
@@ -1,22 +1,34 @@
-import tape from 'tape'
-import server from '../server.js'
+const tape = require('tape')
+const server = require('../server.js')
 
-import endpointHelper from './endpointHelper.js'
-
-tape('Does server respond successfully with the index?', (t) => {
-  server.inject({method: 'GET', url: '/'}, (res) => {
+tape('Does / respond successfully with a correctly set up index?', t => {
+  const options = {
+    method: 'GET',
+    url: '/'
+  }
+  server.inject(options, res => {
     t.equal(res.statusCode, 200, 'Server responds with statusCode: 200')
-    t.equal(res.result.error, undefined, 'Server has no errors')
-    t.ok(res.payload.match('<!DOCTYPE html>'), 'file starts with <!DOCTYPE html>')
     t.ok(res.payload.match('id="container"'), 'index contains the container id')
     t.ok(res.payload.match('<script src="./bundle.js">'), 'index is linking to bundle.js')
     t.end()
   })
 })
 
-endpointHelper('Admin endpoint replys correctly', 'GET', '/admin', 200)
-endpointHelper('Le endpoint replys correctly', 'GET', '/le', 200)
-endpointHelper('Settings endpoint replys correctly', 'GET', '/settings', 200)
-endpointHelper('bundle.js endpoint replys the javascript file correctly', 'GET', '/settings', 200)
-endpointHelper('Wrong endpoint is error handled correctly', 'GET', '/notanendpoint', 404)
-endpointHelper('Wrong method is error handled correctly', 'MET', '/', 404)
+const endpoints = ['admin', 'le', 'settings']
+
+const testingStatusCode = endpoint => {
+  tape('Does server respond successfully with the index?', t => {
+    const options = {
+      method: 'get',
+      url: '/' + endpoint
+    }
+    server.inject(options, res => {
+      t.equal(res.statusCode, 200, endpoint + ' replies with statusCode 200')
+      t.ok(res.payload.indexOf('<!DOCTYPE') > -1)
+      t.end()
+    })
+  })
+}
+
+endpoints.forEach(endpoint => testingStatusCode(endpoint))
+
