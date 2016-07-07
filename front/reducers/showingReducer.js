35183c3856aff7396baac3d4b69ccf77dfd15db1
shouston3 <shouston3@me.com>
2016-07-07 08:52:06 +0100

    Removing files doing any kind of logic or styling (this should come later)

--- front/reducers/showingReducer.js
+++
@@ -1,8 +0,0 @@
-export default function (state = [false, false, false, false, false], action) {
-  switch (action.type) {
-  case 'WORD_SELECTED':
-    return state.map((n, i) => i === action.payload ? !n : n)
-  default:
-    return state
-  }
-}
