2a943899d0ebe5f06977476cac7a0268572e5dd0
Sam Houston <shouston3@me.com>
2016-10-16 21:05:43 +0100

    Adding all the routes with one file, these are all the static views

---
+++ server/routes/index.js
@@ -0,0 +1,25 @@
+const views = ['register', 'login', 'dashboard', 'practice'].map((route) => {
+  return {
+    method: 'get',
+    path: route === 'dashboard' ? '/' : '/' + route,
+    handler: (request, reply) => {
+      reply.view('layout', { component: route });
+    }
+  };
+});
+
+const riot = {
+  method: 'get',
+  path: '/riot+compiler.min.js',
+  handler: (request, reply) => {
+    reply.file('node_modules/riot/riot+compiler.min.js');
+  }
+};
+
+const resources = {
+  method: 'get',
+  path: '/resource/{param*}',
+  handler: { directory: { path: 'public' } }
+};
+
+module.exports = views.concat(riot).concat(resources);
