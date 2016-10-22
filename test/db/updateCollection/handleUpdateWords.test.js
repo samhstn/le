088736fa18ea4eda079d3193109f5dda5351e075
Sam Houston <shouston3@me.com>
2016-10-22 11:10:05 +0100

    Adding testing of more words in the handleUpdateWords helper function ensuring the correction of a previous bug where the same score was being updated in the database

--- test/db/updateCollection/handleUpdateWords.test.js
+++ test/db/updateCollection/handleUpdateWords.test.js
@@ -26,6 +26,16 @@ tape('handleUpdateWords', (t) => {
         direction: 'enToDe',
         source_word: 'hello',
         target_words: ['hallo']
+      },
+      {
+        direction: 'deToEn',
+        source_word: 'die Bibliothek',
+        target_words: ['the library']
+      },
+      {
+        direction: 'deToEn',
+        source_word: 'der Laden',
+        target_words: ['the shop']
       }
     ]
   };
@@ -36,7 +46,22 @@ tape('handleUpdateWords', (t) => {
         direction: 'deToEn',
         source_word: 'hallo',
         target_words: [ 'hello' ],
-        hint: 'my hint'
+        hint: 'my first hint',
+        attempts: 10,
+        correct_attempts: 9,
+        score: 6
+      },  
+      '101': {
+        hint: 'second hint',
+        attempts: 10,
+        correct_attempts: 10,
+        score: 9
+      },  
+      '102': {
+        hint: 'third hint',
+        attempts: 12,
+        correct_attempts: 10,
+        score: 8
       }  
     }
   };
@@ -48,7 +73,7 @@ tape('handleUpdateWords', (t) => {
     .then(() => handleNewWords(pool, initialWordsObj))
     .then(() => getWords('100'))
     .then((res) => {
-      t.equal(res.length, 1);
+      t.equal(res.length, 3);
       t.equal(res[0].word_id, '100');
       t.equal(res[0].collection_id, '100');
       t.equal(res[0].direction, 'enToDe');
@@ -58,12 +83,36 @@ tape('handleUpdateWords', (t) => {
     })
     .then(() => getWords('100'))
     .then((res) => {
-      t.equal(res.length, 1);
+      t.equal(res.length, 3);
       t.equal(res[0].word_id, '100');
       t.equal(res[0].collection_id, '100');
       t.equal(res[0].direction, 'deToEn');
       t.equal(res[0].source_word, 'hallo');
       t.deepEqual(res[0].target_words, [ 'hello' ]);
+      t.equal(res[0].hint, 'my first hint');
+      t.equal(res[0].attempts, '10');
+      t.equal(res[0].correct_attempts, '9');
+      t.equal(res[0].score, 6);
+
+      t.equal(res[1].word_id, '101');
+      t.equal(res[1].collection_id, '100');
+      t.equal(res[1].direction, 'deToEn');
+      t.equal(res[1].source_word, 'die Bibliothek');
+      t.deepEqual(res[1].target_words, [ 'the library' ]);
+      t.equal(res[1].hint, 'second hint');
+      t.equal(res[1].attempts, '10');
+      t.equal(res[1].correct_attempts, '10');
+      t.equal(res[1].score, 9);
+
+      t.equal(res[2].word_id, '102');
+      t.equal(res[2].collection_id, '100');
+      t.equal(res[2].direction, 'deToEn');
+      t.equal(res[2].source_word, 'der Laden');
+      t.deepEqual(res[2].target_words, [ 'the shop' ]);
+      t.equal(res[2].hint, 'third hint');
+      t.equal(res[2].attempts, '12');
+      t.equal(res[2].correct_attempts, '10');
+      t.equal(res[2].score, 8);
       t.end();
     })
     .catch((err) => assert(!err, err));
