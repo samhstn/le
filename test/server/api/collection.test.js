81fe30b44589ed1e9e81e799c6734f7c7fd7eda3
Sam Houston <shouston3@me.com>
2016-10-23 23:52:37 +0100

    Adding testing for the get :: /api/collection/{collection_id} and the delete :: /api/collection/{collection_id} endpoint

--- test/server/api/collection.test.js
+++ test/server/api/collection.test.js
@@ -10,6 +10,7 @@ const authenticate = require('../../helpers/authenticate.js')(pool, redisCli);
 const getCollections = require('../../../db/pg/getCollections.js')(pool);
 const createCollection = require('../../../db/pg/createCollection.js')(pool);
 const getWords = require('../../../db/pg/getWords.js')(pool);
+const updateCollection = require('../../../db/pg/updateCollection.js')(pool);
 
 tape('GET :: /api/collection', (t) => {
   const options = {
@@ -55,6 +56,112 @@ tape('GET :: /api/collection', (t) => {
       t.equal(Object.keys(collections).length, 1);
       t.equal(collections[collectionId].collection_name, 'another name');
       t.equal(collections[collectionId].collection_description, 'another description');
+
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('GET :: /api/collection/{collection_id}', (t) => {
+  const userObj = { username: 'sam', password: 'pass' };
+
+  const collectionObj = {
+    username: 'sam',
+    collection_name: 'another name',
+    collection_description: 'another description'
+  };
+
+  let headers;
+
+  flushDb()
+    .then(() => {
+      const options = {
+        method: 'get',
+        url: '/api/collection/100'
+      };
+
+      return server.inject(options);
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 302);
+      t.equal(res.headers.location, '/login/timeout=true');
+
+      return authenticate(userObj);
+    })
+    .then((_headers) => {
+      headers = _headers;
+
+      return createCollection(collectionObj);
+    })
+    .then(() => {
+      const options = {
+        method: 'get',
+        url: '/api/collection/100'
+      };
+
+      return server.inject(Object.assign(options, { headers }));
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      const collection = JSON.parse(res.payload).collection;
+  
+      t.equal(collection.collection_id, '100');
+      t.equal(collection.collection_name, 'another name');
+      t.equal(collection.collection_description, 'another description');
+      t.equal(collection.words.length, 1);
+
+      const collectionObj = {
+        collection_id: '100',
+        new_words: [
+          {
+            direction: 'deToEn',
+            source_word: 'Wiedersehen',
+            target_words: [ 'Bye' ]
+          },
+          {
+            direction: 'enToDe',
+            source_word: 'hello',
+            target_words: [ 'hallo', 'Guten Tag' ]
+          }
+        ]
+      };
+
+      return updateCollection(collectionObj);
+    })
+    .then(() => {
+      const options = {
+        method: 'get',
+        url: '/api/collection/100'
+      };
+
+      return server.inject(Object.assign(options, { headers }));
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      const collection = JSON.parse(res.payload).collection;
+  
+      t.equal(collection.collection_id, '100');
+      t.equal(collection.collection_name, 'another name');
+      t.equal(collection.collection_description, 'another description');
+
+      t.equal(collection.words.length, 2);
+      t.equal(collection.words[0].word_id, '100');
+      t.equal(collection.words[0].direction, 'deToEn');
+      t.equal(collection.words[0].source_word, 'Wiedersehen');
+      t.deepEqual(collection.words[0].target_words, [ 'Bye' ]);
+      t.equal(collection.words[0].hint, null);
+      t.equal(collection.words[0].attempts, '0');
+      t.equal(collection.words[0].correct_attempts, '0');
+      t.equal(collection.words[0].score, 5);
+
+      t.equal(collection.words[1].word_id, '101');
+      t.equal(collection.words[1].direction, 'enToDe');
+      t.equal(collection.words[1].source_word, 'hello');
+      t.deepEqual(collection.words[1].target_words, [ 'hallo', 'Guten Tag' ]);
+      t.equal(collection.words[1].hint, null);
+      t.equal(collection.words[1].attempts, '0');
+      t.equal(collection.words[1].correct_attempts, '0');
+      t.equal(collection.words[1].score, 5);
 
       t.end();
     })
@@ -192,17 +299,66 @@ tape('PUT :: /api/collection/{collection_id}', (t) => {
     })
     .then((res) => {
       t.equal(res.length, 2);
-      t.equal(res[0].word_id, '100');
-      t.equal(res[0].collection_id, '100');
-      t.equal(res[0].direction, 'enToDe');
-      t.equal(res[0].source_word, 'hello');
-      t.deepEqual(res[0].target_words, [ 'Hallo' ]);
+      t.equal(res.filter((o) => o.word_id === '100')[0].word_id, '100');
+      t.equal(res.filter((o) => o.direction === 'enToDe')[0].direction, 'enToDe');
+      t.equal(res.filter((o) => o.direction === 'enToDe')[0].source_word, 'hello');
+      t.deepEqual(res.filter((o) => o.direction === 'enToDe')[0].target_words, [ 'Hallo' ]);
 
-      t.equal(res[1].word_id, '101');
-      t.equal(res[1].collection_id, '100');
-      t.equal(res[1].direction, 'deToEn');
-      t.equal(res[1].source_word, 'Wiedersehen');
-      t.deepEqual(res[1].target_words, [ 'Bye' ]);
+      t.equal(res.filter((o) => o.word_id === '101')[0].word_id, '101');
+      t.equal(res.filter((o) => o.direction === 'deToEn')[0].direction, 'deToEn');
+      t.equal(res.filter((o) => o.direction === 'deToEn')[0].source_word, 'Wiedersehen');
+      t.deepEqual(res.filter((o) => o.direction === 'deToEn')[0].target_words, [ 'Bye' ]);
+
+      t.end();
+    })
+    .catch((err) => assert(!err, err));
+});
+
+tape('DELETE :: /api/collection/{collection_id}', (t) => {
+  const options = {
+    method: 'delete',
+    url: '/api/collection/100'
+  };
+
+  const userObj = { username: 'sam', password: 'pass' };
+
+  const collectionObj = {
+    username: 'sam',
+    collection_name: 'another name',
+    collection_description: 'another description'
+  };
+
+  let headers;
+
+  flushDb()
+    .then(() => server.inject(options))
+    .then((res) => {
+      t.equal(res.statusCode, 302);
+      t.equal(res.headers.location, '/login/timeout=true');
+
+      return authenticate(userObj);
+    })
+    .then((_headers) => {
+      headers = _headers;
+      
+      return createCollection(collectionObj);
+    })
+    .then(() => getCollections('sam'))
+    .then((res) => {
+      t.deepEqual(Object.keys(res), [ '100' ]);
+      t.equal(res['100'].collection_description, 'another description');
+      t.equal(res['100'].collection_name, 'another name');
+      return server.inject(Object.assign(options, { headers }))
+    })
+    .then((res) => {
+      t.equal(res.statusCode, 200);
+      t.equal(JSON.parse(res.payload).message, 'Collection has been deleted');
+      t.ok(JSON.parse(res.payload).info.deleted);
+
+      return getCollections('sam');
+    })
+    .then((res) => {
+      t.deepEqual(res, {});
 
       t.end();
     })
