a7f4b95decf019a9675b2a2ee53374eda100e5a6
Sam Houston <shouston3@me.com>
2016-10-30 20:02:45 +0000

    Implementing my own tempting engine instead of hacking around with using handlebars in a way it was not meant to be used

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const env2 = require('env2');
 const assert = require('assert');
 const Hapi = require('hapi');
-const Handlebars = require('handlebars');
 
 env2('config.env');
 
 const plugins = require('./plugins/index.js');
 const routes = require('./routes/index.js');
+const views = require('./views.js');
 
 const port = process.env.PORT || 4455;
 
@@ -17,10 +17,7 @@ server.connection({ port });
 server.register(plugins, (err) => {
   assert(!err, err);
 
-  server.views({
-    engines: { html: Handlebars },
-    path: __dirname + '/views'
-  });
+  server.views(views);
 
   server.route(routes);
 });
