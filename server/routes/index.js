fb237d2d4815558dff612bbb3bceb89902c1d83d
Sam Houston <shouston3@me.com>
2016-10-30 23:22:13 +0000

    Drying out the server/routes/index.js file

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,40 +1,26 @@
 const qs = require('querystring');
 
-const createRoute = (route) => {
-  return {
-    method: 'get',
-    path: '/' + route + '/{param?}',
-    config: { auth: false },
-    handler: (request, reply) => {
-      reply.view(route, qs.parse(request.params.param));
-    }
-  };
-};
+const createRoute = (route) => ({
+  path: '/' + route + '/{param?}',
+  handler: (request, reply) => reply.view(route, qs.parse(request.params.param))
+});
 
 const register = createRoute('register');
 const login = createRoute('login');
 
 const riot = {
-  method: 'get',
   path: '/riot+compiler.min.js',
-  config: { auth: false },
-  handler: (request, reply) => {
-    reply.file('node_modules/riot/riot+compiler.min.js');
-  }
+  handler: (request, reply) => reply.file('node_modules/riot/riot+compiler.min.js')
 };
 
 const resources = {
-  method: 'get',
   path: '/resource/{param*}',
-  config: { auth: false },
   handler: { directory: { path: 'public' } }
 };
 
 module.exports = [
   register,
   login,
-  dashboard,
-  practice,
   riot,
   resources
-];
+].map((route) => Object.assign(route, { method: 'get', config: { auth: false } }));
