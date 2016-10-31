fc2add619826a109fc68504db00dc6955c2dd6f8
Sam Houston <shouston3@me.com>
2016-10-31 21:29:08 +0000

    Adding put and del methods to the request helper

--- public/helpers/request.js
+++ public/helpers/request.js
@@ -1,9 +1,9 @@
-(function (exp) {
-  function request (method, url, payload, cb) {
-    var xhr = new XMLHttpRequest();
-    var payloadString = JSON.stringify(payload);
+((exp) => {
+  const request = (method, url, payload, cb) => {
+    const xhr = new XMLHttpRequest();
+    const payloadString = JSON.stringify(payload);
 
-    xhr.onreadystatechange = function () {
+    xhr.onreadystatechange = () => {
       if (xhr.readyState === 4 && xhr.status === 200) {
         cb(xhr.responseText);
       }
@@ -13,16 +13,15 @@
     xhr.send(payloadString);
   };
 
-  var get = function (url, cb) {
-    request('GET', url, null, cb);
-  };
-
-  var post = function (url, payload, cb) {
-    request('POST', url, payload, cb);
-  };
+  const get = (url, cb) => request('GET', url, null, cb);
+  const post = (url, payload, cb) => request('POST', url, payload, cb);
+  const put = (url, payload, cb) => request('PUT', url, payload, cb); 
+  const del = (url, cb) => request('DELETEE', url, null, cb);
 
   exp.request = {
-    get: get,
-    post: post
+    get,
+    post,
+    put,
+    del
   };
 })(window);
