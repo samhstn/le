1fc857a12f7508398f90a263361f2d133ed9ac04
Sam Houston <shouston3@me.com>
2016-11-27 22:32:17 +0000

    Adding tests for the initial state of the dashboard

--- test/riot/dashboard.spec.js
+++ test/riot/dashboard.spec.js
@@ -1,5 +1,30 @@
-describe('first test', function () {
-  it('should be true', function () {
-    expect(true).to.be(true);
+describe('dashboard', function () {
+  context('initial state', function () {
+    before(function () {
+      var dashboard = document.createElement('dashboard');
+      document.body.appendChild(dashboard);
+      riot.mount('dashboard');
+    });
+
+    after(function () {
+      document.body.removeChild(document.querySelector('dashboard'));
+    });
+
+    it('should have a logout button', function () {
+      var logoutButton = document.querySelector('form > button');
+      expect(logoutButton.textContent).to.be('logout');
+    });
+
+    it('should only have the collection view div on the page', function () {
+      expect(document.querySelectorAll('div').length).to.be(1);
+    });
+
+    it('should contain a button with `Add a collection` content', function () {
+      expect(document.querySelector('div > button').textContent).to.be('Add a collection');
+    });
+
+    it('should not display any other tags', function () {
+      expect(document.querySelectorAll('form').length).to.be(1);
+    });
   });
 });
