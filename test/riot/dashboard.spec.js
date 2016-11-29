c98826cc7181e607e26f61d548dbdb15cf0f5f2a
Sam Houston <shouston3@me.com>
2016-11-29 15:48:21 +0000

    Adding tests for the initial state of the dashboard

--- test/riot/dashboard.spec.js
+++ test/riot/dashboard.spec.js
@@ -66,33 +66,157 @@
  * | | | | | context(‘view change’)
  * | | | | | context(‘another edit collection state’) */
 
+function createDashboard () {
+  var dashboard = document.createElement('dashboard');
+  document.body.appendChild(dashboard);
+  [].unshift.call(arguments, 'dashboard');
+  riot.mount.apply(null, arguments);
+}
+
+function removeDashboard () {
+  document.body.removeChild(document.querySelector('dashboard'));
+}
+
+function selectorsTextContent (tag) {
+  return [].map.call(document.querySelectorAll(tag), function (t) {
+    return t.textContent
+  })
+}
+
 describe('dashboard', function () {
-  context('initial state', function () {
-    before(function () {
-      var dashboard = document.createElement('dashboard');
-      document.body.appendChild(dashboard);
-      riot.mount('dashboard');
-    });
+describe('initial states', function () {
+  context('no opts', function () {
+    before(createDashboard);
+    after(removeDashboard);
 
-    after(function () {
-      document.body.removeChild(document.querySelector('dashboard'));
+    it('should only have two buttons', function () {
+      expect(document.querySelectorAll('button').length)
+        .to.be(2);
     });
-
     it('should have a logout button', function () {
-      var logoutButton = document.querySelector('form > button');
-      expect(logoutButton.textContent).to.be('logout');
+      expect(document.querySelector('form > button').textContent)
+        .to.be('logout');
     });
-
-    it('should only have the collection view div on the page', function () {
-      expect(document.querySelectorAll('div').length).to.be(1);
+    it('should only have one div on the page', function () {
+      expect(document.querySelectorAll('div').length)
+        .to.be(1);
     });
-
+    it('should have the collection_view div on the page', function () {
+      expect(document.querySelector('#collection_list_id'))
+        .to.be.an('object');
+    });
     it('should contain a button with `Add a collection` content', function () {
-      expect(document.querySelector('div > button').textContent).to.be('Add a collection');
-    });
-
-    it('should not display any other tags', function () {
-      expect(document.querySelectorAll('form').length).to.be(1);
+      expect(document.querySelector('div > button').textContent)
+        .to.be('Add a collection');
     });
   });
+
+  context('with a single collection', function () {
+    context('default collection state', function () {
+      before(function () {
+        createDashboard({
+          collections: {
+            '100': {
+              collection_name: 'col1',
+              collection_description: 'col1 description',
+              average_score: null,
+              number_of_words: 0
+            }
+          }
+        });
+      });
+      after(removeDashboard);
+      
+      it('should have the right number of divs for one collection', function () {
+        expect(document.querySelectorAll('div').length).to.be(2);
+      });
+      it('should display the collection name in a h4', function () {
+        expect(document.querySelector('h4').textContent)
+          .to.be('col1');
+      });
+      it('should display the collection description in a h6', function () {
+        expect(document.querySelector('h6').textContent)
+          .to.be('col1 description');
+      })
+      it('should display 0 number of words', function () {
+        expect(document.querySelector('p').textContent)
+          .to.be('Number of Words: 0');
+      });
+    });
+
+    context('collection with words state', function () {
+      before(function () {
+        createDashboard();
+        riot.mount('dashboard', {
+          collections: {
+            '100': {
+              collection_name: 'col1',
+              collection_description: 'col1 description',
+              average_score: 5.5,
+              number_of_words: 5
+            }
+          }
+        });
+      });
+      after(removeDashboard);
+
+      it('should have the right number of divs for one collection', function () {
+        expect(document.querySelectorAll('div').length)
+          .to.be(2);
+      });
+      it('should display the collection name in a h4', function () {
+        expect(document.querySelector('h4').textContent)
+          .to.be('col1');
+      });
+      it('should display the collection description in a h6', function () {
+        expect(document.querySelector('h6').textContent)
+          .to.be('col1 description');
+      })
+      it('should display the right words number and score', function () {
+        expect(document.querySelector('p').textContent)
+          .to.be('Average Score: 5.5, Number of Words: 5');
+      });
+    });
+  });
+
+  context('with two collections', function () {
+    before(function () {
+      createDashboard({
+        collections: {
+          '100': {
+            collection_name: 'col1',
+            collection_description: 'col1 description',
+            average_score: 6.6,
+            number_of_words: 3
+          },
+          '101': {
+            collection_name: 'col2',
+            collection_description: 'col2 description',
+            average_score: null,
+            number_of_words: 0
+          }
+        }
+      });
+    });
+    after(removeDashboard);
+
+    it('should have the right number of divs for two collections', function () {
+      expect(document.querySelectorAll('div').length).to.be(3);
+    });
+    it('should display the collection names in a h4', function () {
+      expect(selectorsTextContent('h4'))
+        .to.eql([ 'col1', 'col2' ]);
+    });
+
+    it('should display the collection description in a h6', function () {
+      expect(selectorsTextContent('h6'))
+        .to.eql([ 'col1 description', 'col2 description' ]);
+    })
+
+    it('should display the right words number and score', function () {
+      expect(selectorsTextContent('p'))
+        .to.eql([ 'Average Score: 6.6, Number of Words: 3', 'Number of Words: 0' ]);
+    });
+  });
+});
 });
